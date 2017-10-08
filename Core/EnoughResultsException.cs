43b57af427ab4ff68969ecd6eb665503be55ed58
YoungForest <yangsen758@foxmail.com>
2017-10-08 17:15:13 +0800

    Add Exception Class Enough ResultsException.

---
+++ Core/EnoughResultsException.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Core
+{
+    [Serializable]
+    internal class EnoughResultsException : Exception
+    {
+        public EnoughResultsException()
+        {
+        }
+
+        public EnoughResultsException(string message) : base(message)
+        {
+        }
+
+        public EnoughResultsException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected EnoughResultsException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+
+        public override string ToString()
+        {
+            return "Enough sudoku get!";
+        }
+    }
+}
