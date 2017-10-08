4a4ff5d6cbd5b028ad0662a6934cea1c965ab3ce
YoungForest <yangsen758@foxmail.com>
2017-10-08 18:53:43 +0800

    Add comment for a few methods.

--- Core/Generator.cs
+++ Core/Generator.cs
@@ -17,7 +17,11 @@ namespace Core
         public int bound = 0;
         public List<int[,]> results = new List<int[,]>();
 
-
+        /// <summary>
+        /// 填充数独的下一个格子
+        /// </summary>
+        /// <param name="i">格子位置：第i行</param>
+        /// <param name="j">格子位置：第j列</param>
         public void FillNextGrid(int i, int j)
         {
             var fillList = SudokuFounctionLibrary.GenerateFillList();
@@ -42,6 +46,9 @@ namespace Core
             });
         }
 
+        /// <summary>
+        /// 打印结果到 results数组中
+        /// </summary>
         private void PrintResult()
         {
             results.Add(grid);
